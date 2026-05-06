d7ae396d6862acff04d2a8faca6b26813aefaf21
Antonio Chihuailaf <antonio.caddin@gmail.com>
2026-05-05 21:50:24 -0400

    feat: API key protection y URL Railway

--- Producto/FarmaCode-Frontend/app/src/main/java/com/farmacox/farmacode/data/network/RetrofitClient.kt
+++ Producto/FarmaCode-Frontend/app/src/main/java/com/farmacox/farmacode/data/network/RetrofitClient.kt
@@ -8,12 +8,20 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://10.172.39.30:8080/"
+    private const val BASE_URL = "https://farmacode-production-c60c.up.railway.app/"
+
+    private const val API_KEY = "farmacode-secret-2026"
 
     private val httpClient = OkHttpClient.Builder()
         .addInterceptor(HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
         })
+        .addInterceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader("X-Api-Key", API_KEY)
+                .build()
+            chain.proceed(request)
+        }
         .connectTimeout(30, TimeUnit.SECONDS)
         .readTimeout(60, TimeUnit.SECONDS)
         .writeTimeout(30, TimeUnit.SECONDS)
